a2ae4d9515a2eea047159a00015bfa2ff2b53f04
A2nis <adonisfernandezr@gmail.com>
2022-10-27 15:09:59 -0400

    updated

--- pages/components/storieCard.jsx
+++ pages/components/storieCard.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./StorieCard.module.css";
 function StorieCard({ imagen, date, title, author, textLink, link }) {
   return (
@@ -11,18 +12,24 @@ function StorieCard({ imagen, date, title, author, textLink, link }) {
           {author && <p className={styles.sAuthor}>{author}</p>}
           <hr className={styles.storieHr} />
           {textLink && (
-            <a href={link}>
-              <div className={styles.sLink}>
-                <h3 className={styles.sTextLink}>{textLink}</h3>
-                <div className={styles.sArrow}>
-                  <Image
-                    src="/assets/shared/desktop/arrow1.svg"
-                    alt="arrow"
-                    layout="fill"
-                  />
-                </div>
+            <div className={styles.sLink}>
+              <Link href={link || ""}>
+                <a>
+                  <h3 className={styles.sTextLink}>{textLink}</h3>
+                </a>
+              </Link>
+              <div className={styles.sArrow}>
+                <Link href={link || ""}>
+                  <a>
+                    <Image
+                      src="/assets/shared/desktop/arrow1.svg"
+                      alt="arrow"
+                      layout="fill"
+                    />
+                  </a>
+                </Link>
               </div>
-            </a>
+            </div>
           )}
         </div>
       </article>
